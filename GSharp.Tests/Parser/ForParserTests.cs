7f0bc3c524a4663902d2c1427a9c6996a78a26a2
greg <gregory.wow@hotmail.com>
2025-05-01 22:34:35 -0300

    create forparser testss

--- GSharp.Tests/Parser/ForParserTests.cs
+++ GSharp.Tests/Parser/ForParserTests.cs
@@ -1,11 +1,94 @@
 using FluentAssertions;
+using GSharp.Parser;
 using GSharp.AST;
 using GSharp.Lexer;
-using GSharp.Parser;
 
 namespace G.Sharp.Compiler.Tests.Parser;
 
 public class ForParserTests
 {
-   
+    [Fact]
+    public void Should_Parse_Valid_For_Loop()
+    {
+        var tokens = new List<Token>
+        {
+            new(TokenType.For, "for"),
+            new(TokenType.Identifier, "item"),
+            new(TokenType.In, "in"),
+            new(TokenType.Identifier, "nums"),
+            new(TokenType.LeftBrace, "{"),
+            new(TokenType.Println, "println"),
+            new(TokenType.Identifier, "item"),
+            new(TokenType.Semicolon, ";"),
+            new(TokenType.RightBrace, "}"),
+            new(TokenType.EndOfFile, "")
+        };
+
+        var parser = new GSharp.Parser.Parser(tokens);
+        var result = new ForParser(parser).Parse();
+
+        result.Variable.Should().Be("item");
+        result.Iterable.Should().BeOfType<VariableExpression>();
+        result.Body.Should().ContainSingle().Which.Should().BeOfType<PrintStatement>();
+    }
+
+    [Fact]
+    public void Should_Throw_When_Missing_In()
+    {
+        var tokens = new List<Token>
+        {
+            new(TokenType.For, "for"),
+            new(TokenType.Identifier, "item"),
+            new(TokenType.Identifier, "nums"),
+            new(TokenType.LeftBrace, "{"),
+            new(TokenType.RightBrace, "}"),
+            new(TokenType.EndOfFile, "")
+        };
+
+        var parser = new GSharp.Parser.Parser(tokens);
+        var act = () => new ForParser(parser).Parse();
+
+        act.Should().Throw<Exception>().WithMessage("*Expected token In*");
+    }
+
+    [Fact]
+    public void Should_Throw_When_Missing_LeftBrace()
+    {
+        var tokens = new List<Token>
+        {
+            new(TokenType.For, "for"),
+            new(TokenType.Identifier, "item"),
+            new(TokenType.In, "in"),
+            new(TokenType.Identifier, "nums"),
+            new(TokenType.RightBrace, "}"),
+            new(TokenType.EndOfFile, "")
+        };
+
+        var parser = new GSharp.Parser.Parser(tokens);
+        var act = () => new ForParser(parser).Parse();
+
+        act.Should().Throw<Exception>().WithMessage("*Expected token LeftBrace*");
+    }
+
+    [Fact]
+    public void Should_Parse_For_With_Empty_Body()
+    {
+        var tokens = new List<Token>
+        {
+            new(TokenType.For, "for"),
+            new(TokenType.Identifier, "item"),
+            new(TokenType.In, "in"),
+            new(TokenType.Identifier, "nums"),
+            new(TokenType.LeftBrace, "{"),
+            new(TokenType.RightBrace, "}"),
+            new(TokenType.EndOfFile, "")
+        };
+
+        var parser = new GSharp.Parser.Parser(tokens);
+        var result = new ForParser(parser).Parse();
+
+        result.Variable.Should().Be("item");
+        result.Iterable.Should().BeOfType<VariableExpression>();
+        result.Body.Should().BeEmpty();
+    }
 }
